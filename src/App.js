67f6d608b8ff37216d73f80e3c4f32dd1424b0f4
Raveendra Tudangil <raveendra.tudangil@gmail.com>
2020-05-10 11:43:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    state = {displayBio: false};
+    toggleDisplyBio = () => {
+        this.setState({displayBio: !this.state.displayBio});
+    }
+
+    // constructor(){
+    //     super();
+    //     this.toggleDisplyBio = this.toggleDisplyBio.bind(this);
+    //     this.toggleDisplyBio = () =>{...};
+    // }
+    
+
+    render () {
+        return(
+            <div>
+                <img src={profile} alt='profile' className='profile'/>
+                <h1>Hello!</h1>
+                <p>My name is Raveendra. I'm a software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                this.state.displayBio ? (
+                    <div>
+                        <p>I live in Mumbai, and code everyday</p>
+                        <p>My favourite language is JavaScript, and I think Ract.js is awesome.</p>
+                        <p>Besides coding, I also love music and ramen!</p>
+                        <button onClick={this.toggleDisplyBio}>Show less</button>
+                    </div>
+                    ):
+                    (<div>
+                        <button onClick={this.toggleDisplyBio}>Read more</button>
+                    </div>
+                )
+                }
+                 <hr />
+                 <Projects/>
+                 <hr/>
+                 <SocialProfiles />
+            </div>
+        )
+    }
 }
-
 export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+import project1 from '../assets/project1.png'
+import project2 from '../assets/project2.png'
+import project3 from '../assets/project3.png'
+
+const PROJECTS =[
+
+    {
+        id : 1,
+        title : 'Example React Application',
+        description : 'A React App that I built, involving JS and core web dev concepts!',
+        link : 'https://www.udemy.com/course/react-js-and-redux-mastering-web-apps/',
+        image : project1
+    },
+    {
+        id : 2,
+        title : 'My API',
+        description : 'A REST API that I built form scrach with GET and POST requests!',
+        link : 'https://www.udemy.com/course/react-js-and-redux-mastering-web-apps/',
+        image : project2
+    },
+    {
+        id : 3,
+        title : 'Operating Systems Final Project',
+        description : 'My uniqueue final project for my university Operating systems course.',
+        link : 'https://www.udemy.com/course/react-js-and-redux-mastering-web-apps/',
+        image : project3
+    }
+];
+
+export default PROJECTS;
